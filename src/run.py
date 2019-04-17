301f89e93fd1ccf76c55a10c7b304f971be47663
Henry Wilde <henrydavidwilde@gmail.com>
2019-04-17 23:14:08 +0100

    Move to recursive `run.py`

--- src/run.py
+++ src/run.py
@@ -5,7 +5,15 @@ import os
 import sys
 
 
+def get_arguments(args, idx):
+    """ Get the set of arguments at index `idx`. """
+
+    iter_args = iter(itertools.islice(args, idx, None))
+    return next(iter_args, "STOP")
+
+
 def main(
+    idx=0,
     num_cores=8,
     sizes=[100, 500, 1000],
     mutations=[0.001, 0.01, 0.05],
@@ -14,22 +22,53 @@ def main(
     """ Write a bash script with the parameters for a series of runs. """
 
     cases = ["bounded", "unbounded"]
+    args = itertools.product(
+        [num_cores], cases, sizes, mutations, range(repetitions)
+    )
+
+    arguments = get_arguments(args, idx)
+
+    if arguments == "STOP":
+        return
+
+    cores, case, size, mutation, seed = arguments
     with open("run.sh", "w") as bash:
-        for cores, case, size, mutation, seed in itertools.product(
-            [num_cores], cases, sizes, mutations, range(repetitions)
-        ):
-            bash.write(
-                f"python main.py {cores} {case} {size} {mutation} {seed}\n"
-            )
+
+        JOBSCRIPT = "!/bin/bash --login\n\n"\
+            + f"#SBATCH -J edo_kmeans_{case}\n"\
+            + f"#SBATCH --ntasks={cores}\n"\
+            + f"#SBATCH --ntasks-per-node={cores}\n"\
+            + "#SBATCH -A scw1337\n"\
+            + "#SBATCH -p compute\n"\
+            + "#SBATCH -t 23:59:59\n"\
+            + "#SBATCH --exclusive\n\n"
+        bash.write(JOBSCRIPT)
+
+        bash.write("export THIS_SCRATCH=/scratch/$USER/$SLURM_JOBID\n")
+        bash.write("rm -rf $THIS_SCRATCH\n")
+        bash.write("mkdir -p $THIS_SCRATCH\n")
+        bash.write("cd $THIS_SCRATCH\n")
+        bash.write("cp -r $SLURM_SUBMIT_DIR/.. .\n\n")
+
+        bash.write("module load anaconda\n")
+        bash.write("conda env create -f ../environment.yml\n")
+        bash.write("source activate edo-kmeans\n\n")
+
+        bash.write("cd src\n")
+        bash.write(f"python main.py {cores} {case} {size} {mutation} {seed}\n")
 
     os.system("chmod +x run.sh")
+    os.system("./run.sh")
+
+    main(idx + 1, num_cores, sizes, mutations, repetitions)
 
 
 if __name__ == "__main__":
 
-    NUM_CORES = sys.argv[1]
-    SIZES = list(sys.argv[2].split(","))
-    MUTATIONS = list(sys.argv[3].split(","))
-    REPETITIONS = int(sys.argv[4])
+    IDX = int(sys.argv[1])
+    NUM_CORES = sys.argv[2]
+    SIZES = list(sys.argv[3].split(","))
+    MUTATIONS = list(sys.argv[4].split(","))
+    REPETITIONS = int(sys.argv[5])
 
-    main(NUM_CORES, SIZES, MUTATIONS, REPETITIONS)
+    main(IDX, NUM_CORES, SIZES, MUTATIONS, REPETITIONS)
