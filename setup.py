74a5d27c251d1a082c57b6a415891b574d586b1a
Henry Wilde <henrydavidwilde@gmail.com>
2019-06-04 14:29:09 +0100

    v0.0.1

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 """ Setup script. """
 
+# pylint: disable=exec-used,undefined-variable
+
 from setuptools import find_packages, setup
+
+with open("README.rst", "r") as readme_file:
+    README = readme_file.read()
+
+exec(open("src/edo_exp/version.py", "r").read())
 
 setup(
     name="edo_exp",
-    version="0.0.1",
+    version=__version__,
     description="Generic functions for running experiments with `edo`.",
+    long_description=README,
     url="https://github.com/daffidwilde/edo_exp",
     author="Henry Wilde",
     author_email="henrydavidwilde@gmail.com",
